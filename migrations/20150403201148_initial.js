8de6f27ab21fc1b47a32e8578fce7073d677701a
HarleyKwyn <kwyn.meagher@gmail.com>
2015-04-04 16:52:04 -0700

    Fix migration format

--- migrations/20150403201148_initial.js
+++ migrations/20150403201148_initial.js
@@ -1,40 +1,35 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-  return Promise.join(
+  return knex.schema
     // Create emails table
-    knex.schema.createTable('emails', function (table) {
-      table.increments('id').primary();
-      table.string('email');
+    .createTable('emails', function (table) {
+      table.increments('email_id').primary();
+      table.string('email').unique();
       table.timestamps();
-    }),
-
+    })
     // Create tags table
-    knex.schema.createTable('tags', function (table){
-      table.increments('id').primary();
-      table.string('tag_name');
-    }),
+    .createTable('tags', function (table){
+      table.increments('tag_id').primary();
+      table.string('tag_name').unique();
+    })
     // Create join table for tags and emails
-    knex.schema.createTable('tag_map', function (table){
+    .createTable('tag_map', function (table){
       table.increments().primary();
       table.integer('email_id')
         .unsigned()
-        .references('id')
-        .inTable('emails');
+        .references('email_id')
+        // .inTable('emails.id');
       table.integer('tag_id')
         .unsigned()
-        .references('id')
-        .inTable('tags');
-    })
-  );
+        .references('tag_id')
+        // .inTable('tags');
+    });
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.join(
-    // knex.raw('SET foreign_key_checks = 0;'),
-    knex.schema.dropTable('tag_map'),
-    knex.schema.dropTable('emails'),
-    knex.schema.dropTable('tags')
-    // knex.raw('SET foreign_key_checks = 1;')
-  );
+  return knex.schema
+          .dropTable('tag_map')
+          .dropTable('emails')
+          .dropTable('tags')
 };
