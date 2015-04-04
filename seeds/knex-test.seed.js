679620ddf5fa1b076338ed19c859e15e81e04287
HarleyKwyn <kwyn.meagher@gmail.com>
2015-04-04 16:52:24 -0700

    Fix seed file

--- seeds/knex-test.seed.js
+++ seeds/knex-test.seed.js
@@ -4,26 +4,23 @@ var knex = require('knex')(config.database);
 // For testing
 exports.seed = function() {
     return Promise.join(
-        // Deletes ALL existing entries
-        knex('tag_map').del(),
-        knex('tags').del(),
-        knex('emails').del()
-        ).then(
-        // Inserts seed entries
-        // insert test emails
-        knex.insert({id: 1, email: 'tagged@test.com'})
-            .into('emails')
-            .insert({id: 2, email: 'untagged@test.com'})
-            .into('emails')
+            // Deletes ALL existing entries
+            knex('tag_map').del(),
+            knex('tags').del(),
+            knex('emails').del()
+        )
+        .then(Promise.join(
+            // Inserts seed entries
+            // insert test emails
+            knex('emails').insert({email_id: 1, email: 'tagged@test.com'}),
+            knex('emails').insert({email_id: 2, email: 'untagged@test.com'}),
             // Insert tags
-            .insert({id: 1, tag_name: 'tag1'})
-            .into('tags')
-            .insert({id: 2, tag_name: 'tag2'})
-            .into('tags')
-        // Map tags to tagged
-            .insert({id: 1, email_id: 1, tag_id: 1})
-            .into('tag_map')
-            .insert({id: 2, email_id: 1, tag_id: 2})
-            .into('tag_map')
-        );
+            knex('tags').insert({tag_id: 1, tag_name: 'tag1'}),
+            knex('tags').insert({tag_id: 2, tag_name: 'tag2'})
+        ))
+        .then(Promise.join(
+            // Map tags to tagged
+            knex('tag_map').insert({id: 1, email_id: 1, tag_id: 1}),
+            knex('tag_map').insert({id: 2, email_id: 1, tag_id: 2})
+        ));
 };
